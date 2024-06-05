1db9bc8a5cd8e2a5c6586bac7983874618b8c088
kcmaake <kgaugelomc.com>
2024-06-05 12:30:46 +0200

    added functions for code reusability

--- movie_collection.py
+++ movie_collection.py
@@ -1,20 +1,62 @@
 print("Welcome!\n")
 
-user_choice = input("Type 'a' to add new movie, 'v' to view movie collection or 'q' to exit: ").lower()
-
 movies = []
 
-while user_choice != 'q':
-  if user_choice == 'a':
-    user_movie = input("\nEnter movie name: ")
+
+def main_menu():
+  menu = "\nMenu\n--------------------\n1. Add movie\n2. View collection\n3. Search movie\n4. Exit"
+  print(menu)
+
+  app_operations()
+
+
+def navigate():
+  nav_menu = "\n1. Add another movie\n2. Main menu\n3. Exit"
+  print(nav_menu)
+  user_nav = int(input("\nSelect option: "))
+
+  if user_nav == 1:
+    user_movie = input("\nEnter name of movie: ").lower()
     movies.append(user_movie)
 
-    user_choice = input("\nType 'a' to add new movie, 'v' to view movie collection or 'q' to exit: ").lower()
-  elif user_choice == 'v':
-    print(movies)
-
-    user_choice = input("\nType 'a' to add new movie, 'v' to view movie collection or 'q' to exit: ").lower()
+    navigate()
+  elif user_nav == 2:
+    main_menu()
+  elif user_nav == 3:
+    exit()
   else:
-    print("\nInvalid option")
-    
-    user_choice = input("\nType 'a' to add new movie, 'v' to view movie collection or 'q' to exit: ").lower()
+    print("\nInvalid option!")
+    navigate() 
+
+
+def app_operations():
+  user_choice = int(input("\nSelect option: "))
+
+  while user_choice != 4:
+    if user_choice == 1:
+      user_movie = input("\nEnter movie name: ").lower()
+      movies.append(user_movie)
+      print("\nMovie added!")
+
+      navigate()
+    elif user_choice == 2:
+      print(movies)
+
+      navigate()
+    elif user_choice == 3:
+      search_movies()
+    else:
+      print("\nInvalid option")
+
+      main_menu()
+
+
+def search_movies():
+  user_search_query = input("\nEnter movie you want to search for: ").lower()
+ 
+  if user_search_query in movies:
+    print("\nMovie found")
+    navigate()
+  else:
+    print("\nMovie not found!")
+    navigate()
